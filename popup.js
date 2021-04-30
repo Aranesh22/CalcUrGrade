f673e0a7b7f96990d79d1fbfcb66a6b75e2b4eff
rishiekugathasan <rishie2001@gmail.com>
2021-04-30 15:12:37 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -51,6 +51,29 @@ function del() {
     calculateMark();
 }
 
+function projected(currentMark, finalWeight, desiredMark) {
+
+    if (currentMark < 0 || currentMark > 100 || finalWeight < 0 || finalWeight > 100 || desiredMark < 0 || desiredMark > 100) {
+        console.log("Invalid values were entered.");
+        return;
+    }
+
+    currentMark = currentMark/100;
+    finalWeight = finalWeight/100;
+    desiredMark = desiredMark/100;
+
+    let calculation = (desiredMark - (1 - finalWeight) * currentMark) / finalWeight;
+    calculation *= 100;
+    calculation = calculation.toFixed(2);
+    
+    return calculation;
+}
+
+/* Projected function tests
+projected(70,50,80);
+console.log(projected(85,20,90));
+*/
+
 /*
 Test_1 - calculateMark
 add(80,50);
